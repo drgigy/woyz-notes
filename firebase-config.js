3069a24c880979cab86271a8ee38bcf7d86cb8be
Gigy Kuruttukulam <gigy@Gigys-MacBook-Air.local>
2026-08-07 13:13:07 +0530

    Initial WOYZ Notes application

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Console > Project settings > Your apps > Web app > SDK setup.
+// This configuration identifies your Firebase project; Firestore rules protect the data.
+export const firebaseConfig = {
+  apiKey: "PASTE_FIREBASE_WEB_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
+  appId: "PASTE_FIREBASE_APP_ID"
+};
